b2b47416b0b8edab4088ab7961acf67f3d18234b
Zhenfeng Liao <86788232+biabiabia7@users.noreply.github.com>
2022-06-20 16:33:52 -0400

    Time: 6722 ms (5.03%), Space: 15.6 MB (75.66%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        count = 0
+        hashmap = {}
+        for word in words:
+            if word not in hashmap:
+                if self._isSubsequence(word, s):
+                    hashmap[word] = True
+                    count += 1
+                else:
+                    hashmap[word] = False
+            else:
+                count += hashmap[word]
+        return count
+        
+        
+    def _isSubsequence(self, sub: str, t: str) -> bool:
+        if len(sub) > len(t):
+            return False
+        
+        ps, pt = 0, 0
+        while pt < len(t) and ps < len(sub):
+            if sub[ps] == t[pt]:
+                
+                ps += 1
+                pt += 1
+            else:
+                pt += 1
+        return ps == len(sub)
